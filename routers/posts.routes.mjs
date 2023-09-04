9655e192b636fd44c7896b2d3b4f2d550c20b75e
Anyamah Ndidi <anyamahedwin@gmail.com>
2023-09-04 15:07:24 +0100

    get all post and single post implemented

--- routers/posts.routes.mjs
+++ routers/posts.routes.mjs
@@ -1,7 +1,9 @@
 import express from "express";
 
 const router = express.Router()
-import {addUserPost} from "../controller/posts.controller.mjs"
+import {addUserPost, getAllUserPost,getSingleUserPosts} from "../controller/posts.controller.mjs"
 
 router.post("/addpost/:id", addUserPost)
+router.get("/all/post", getAllUserPost)
+router.get("/single/post/:id", getSingleUserPosts)
 export default router
